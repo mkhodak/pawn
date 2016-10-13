4d344dde2abb6ee27bd02e6fe6b62b5f095bc85e
Mikhail Khodak <mkhodak@spin.cs.princeton.edu>
2016-10-12 23:13:11 -0400

    initial code

---
+++ pawn/load.py
@@ -0,0 +1,95 @@
+from collections import defaultdict
+from functools import partial, wraps
+try:
+	import ujson as json
+except ImportError:
+	import json
+
+
+_datapath = './pawn/data/'
+_language = 'en'
+
+
+# caching wrapper to store language dictionaries
+def _language_cache(func, names):
+	cache = {'en': {name: {} for name in names}}
+	@wraps(func)
+	def wrapper(*args, **kwargs):
+		if _language in cache:
+			for name in names:
+				globals()[name] = cache[_language][name]
+		else:
+			func(*args, **kwargs)
+			cache[_language] = {name: globals()[name] for name in names}
+	return wrapper
+
+
+# load word-synset matching data
+@partial(_language_cache, names=['_word2synsets'])
+def _load_word2synsets():
+	global _word2synsets
+	with open(_datapath+_language+'_data.json', 'r') as f:
+		_word2synsets = json.load(f)
+
+
+# load word count data
+@partial(_language_cache, names=['_wordcounts'])
+def _load_wordcounts():
+	global _wordcounts
+	with open(_datapath+_language+'_vocab.txt', 'r') as f:
+		_wordcounts = {word: int(count) 
+					   for word, count in (line.split() for line in f)}
+
+
+# sets dictionaries for mapping synsets between languages and synsets to lemmas
+@partial(_language_cache, names=['_synset2lemmas', 
+								 '_synset2pwn', 
+								 '_synset2lang'])
+def _set_synsetmaps():
+	global _synset2lemmas
+	global _synset2pwn
+	global _synset2lang
+	synset2lemmas = defaultdict(lambda: [])
+	for word, synsets in _word2synsets.items():
+		for synset in synsets:
+			synset2lemmas[synset].append(word)
+	_synset2lemmas = {}
+	_synset2pwn = {}
+	_synset2lang = {}
+	wordcounts = bool(_wordcounts)
+	for synset, lemmas in sorted(synset2lemmas.items()):
+		if wordcounts:
+			sortedlemmas = sorted(lemmas, 
+								  key=_wordcounts.__getitem__, 
+								  reverse=True)
+		else:
+			sortedlemmas = sorted(lemmas)
+		name = '.'.join([sortedlemmas[0], synset.split('.')[-2], '01'])
+		index = 1
+		while name in _synset2pwn:
+			index += 1
+			name[-2:] = (index<10)*'0'+str(index)
+		_synset2pwn[name] = synset
+		_synset2lang[synset] = name
+		_synset2lemmas[name] = ['.'.join([name, lemma]) 
+								for lemma in sortedlemmas]
+
+
+# reload and reset all dictionaries
+def _reset():
+	_load_word2synsets()
+	_load_wordcounts()
+	_set_synsetmaps()
+
+
+def language():
+	"""get current language setting"""
+	return _language
+
+
+def set_language(language):
+	"""reset language setting"""
+	global _language
+	if _language != language:
+		_language = language
+		_reset()
---
+++ pawn/wordnet.py
@@ -0,0 +1,195 @@
+from collections import defaultdict
+from nltk.corpus import wordnet as wn
+from nltk.corpus.reader.wordnet import Lemma
+from nltk.corpus.reader.wordnet import Synset
+from nltk.corpus.reader.wordnet import WordNetCorpusReader
+from nltk.corpus.reader.wordnet import WordNetError
+import pawn.load as data
+
+
+_indicator = '_'
+_dummy = 'DUMMY~'
+
+
+###############################################################################
+################ Synset Wrapper, Replacement, and Access Methods ##############
+###############################################################################
+
+
+# updates synset based on current language
+def _synset_update(synset):
+	language = data._language
+	if dir(synset)[0] == _indicator:
+		if synset._language != language:
+			PWN_name = synset._PWN_name
+			if language == 'en':
+				setattr(synset, '_name', PWN_name)
+			else:
+				setattr(synset, '_name', 
+						data._synset2lang.get(PWN_name, _dummy+PWN_name))
+			setattr(synset, '_language', language)
+	else:
+		PWN_name = synset._name
+		setattr(synset, '_PWN_name', PWN_name)
+		if language != 'en':
+			setattr(synset, '_name', 
+					data._synset2lang.get(PWN_name, _dummy+PWN_name))
+		setattr(synset, _indicator, None)
+		setattr(synset, '_language', language)
+
+
+# wrapper to update class 'Synset' before function execution
+def _synset_wrapper(func, nargs):
+	if nargs == 1:
+		def wrapper(self):
+			_synset_update(self)
+			return func(self)
+	elif nargs == 2:
+		def wrapper(self, other):
+			_synset_update(self)
+			_synset_update(other)
+			return func(self, other)
+	return wrapper
+
+
+def synset(name):
+	"""emulates wn.synset"""
+	if data._language != 'en':
+		if name[:len(_dummy)] == _dummy:
+			name = name[len(_dummy):]
+		else:
+			name = data._synset2pwn[name]
+	return wn.synset(name)
+
+
+def synsets(token, pos='anrsv'):
+	"""emulates wn.synsets"""
+	if data._language == 'en':
+		return wn.synsets(name)
+	return [wn.synset(name) 
+			for name in data._word2synsets[token] 
+			if name.split('.')[-2] in pos]
+
+
+###############################################################################
+################ Lemma Wrapper, Replacement, and Access Methods ###############
+###############################################################################
+
+
+# instantiation of Lemma object
+def _lemma(name, synset):
+	language = data._language
+	synset_name = synset.name()
+	for lemma_name, lemma in _lemmas[language][synset_name].items():
+		if name == lemma_name:
+			return lemma
+	lemma = Lemma(WordNetCorpusReader, synset, name, 0, 0, None)
+	_lemmas[language][synset_name][name] = lemma
+	return lemma
+
+
+# replacement for 'lemmas' method of class 'Synset'
+def _synset_lemmas(self):
+	if data._language == 'en':
+		return self._lemmas
+	return [_lemma(name, self) 
+			for name in data._synset2lemmas.get(self.name(), [])]
+
+
+# replacement for 'lemma_names' method of class 'Synset'
+def _synset_lemma_names(self):
+	if data._language == 'en':
+		return self._lemma_names
+	return [name for name in data._synset2lemmas.get(self.name(), [])]
+
+
+# wrapper for lexical relation functions of class 'Lemma'
+def _lexical_relation_wrapper(func):
+	def wrapper(self):
+		if data._language == 'en':
+			return func(self)
+		return sum((lemma2.synset().lemmas() 
+					for lemma1 in wn.synset(self._PWN_name)._lemmas 
+					for lemma2 in func(lemma1)), [])
+	return wrapper
+
+
+# replacement for 'count' method of class 'Lemma'
+def _lemma_count(self):
+	if data._language == 'en':
+		return self.count()
+	return len(data._word2synsets[self._name])
+
+
+def lemma(name):
+	"""emulates wn.lemma"""
+	if data._language == 'en':
+		return wn.lemma(name)
+
+	chunks = name.split('.')
+	i = 3
+	success = 0
+	while not success:
+		try:
+			synsetname = '.'.join(chunks[:i])
+			lemmas = data._synset2lemmas[synsetname]
+			success = 1
+		except KeyError:
+			i += 1
+	if name in lemmas:
+		lemmaname = '.'.join(chunks[i:])
+		return _lemma(lemmaname, synset(synsetname))
+	raise WordNetError('Lemma ' + name + ' Not Found')
+
+
+def lemmas(token, pos='anrsv'):
+	"""emulates wn.lemmas"""
+	if data._language == 'en':
+		return wn.lemmas(token, pos)
+	return [_lemma(token, wn.synset(name)) 
+			for name in data._word2synsets[token] 
+			if name.split('.')[-2] in pos]
+
+
+###############################################################################
+################ Initialization and Function Wrapping #########################
+###############################################################################
+
+
+def _wrap_class_method(Object, method, wrapper, *args):
+	setattr(Object, method, wrapper(getattr(Object, method), *args))
+
+
+def all_lemma_names():
+	if data._language == 'en':
+		return wn.all_lemma_names()
+	return data._wordcounts.keys()
+
+
+def all_synsets():
+	if data._language == 'en':
+		return wn.all_synsets()
+	return (wn.synset(synset) for synset in data._synset2lemmas.keys())
+
+
+_lemmas = defaultdict(lambda: defaultdict(lambda: {}))
+
+_wrap_class_method(Synset, 'name', _synset_wrapper, 1)
+_wrap_class_method(Synset, '__repr__', _synset_wrapper, 1)
+_wrap_class_method(Synset, '__hash__', _synset_wrapper, 1)
+_wrap_class_method(Synset, '__eq__', _synset_wrapper, 2)
+_wrap_class_method(Synset, '__ne__', _synset_wrapper, 2)
+_wrap_class_method(Synset, '__lt__', _synset_wrapper, 2)
+setattr(Synset, 'lemmas', _synset_lemmas)
+setattr(Synset, 'lemma_names', _synset_lemma_names)
+
+_wrap_class_method(Lemma, 'antonyms', _lexical_relation_wrapper)
+_wrap_class_method(Lemma, 'pertainyms', _lexical_relation_wrapper)
+setattr(Lemma, 'count', _lemma_count)
+
+lemma_count = lambda lemma: lemma.count()
+words = all_lemma_names
+wn.ensure_loaded()
+for attr in dir(wn):
+	if attr != '_' and not attr in globals():
+		globals()[attr] = getattr(wn, attr)
