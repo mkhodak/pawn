0c8baf4408108084a83cabf8ce3caf3f28bb7ff0
Mikhail Khodak <mkhodak@spin.cs.princeton.edu>
2017-08-19 21:45:03 -0400

    fixed treetagger bug

--- pawn/load.py
+++ pawn/load.py
@@ -2,9 +2,9 @@ from collections import defaultdict
 from functools import partial
 from functools import wraps
 try:
-	import ujson as json
+  import ujson as json
 except ImportError:
-	import json
+  import json
 from pawn.morphy import Morphy
 
 
@@ -15,78 +15,75 @@ _language = 'en'
 
 # caching wrapper to store language dictionaries
 def _language_cache(func, names):
-	cache = {'en': {name: {} for name in names}}
-	@wraps(func)
-	def wrapper(*args, **kwargs):
-		if _language in cache:
-			for name in names:
-				globals()[name] = cache[_language][name]
-		else:
-			func(*args, **kwargs)
-			cache[_language] = {name: globals()[name] for name in names}
-	return wrapper
+  cache = {'en': {name: {} for name in names}}
+  @wraps(func)
+  def wrapper(*args, **kwargs):
+    if _language in cache:
+      for name in names:
+        globals()[name] = cache[_language][name]
+    else:
+      func(*args, **kwargs)
+      cache[_language] = {name: globals()[name] for name in names}
+  return wrapper
 
 
 # load word-synset matching data
 @partial(_language_cache, names=['_word2synsets'])
 def _load_word2synsets():
-	global _word2synsets
-	with open(_datapath+_language+'_data.json', 'r') as f:
-		_word2synsets = json.load(f)
+  global _word2synsets
+  with open(_datapath+_language+'_data.json', 'r') as f:
+    _word2synsets = json.load(f)
 
 
 # load word count data
 @partial(_language_cache, names=['_wordcounts'])
 def _load_wordcounts():
-	global _wordcounts
-	global _words
-	with open(_datapath+_language+'_vocab.txt', 'r') as f:
-		_wordcounts = {word: int(count) for word, count in (line.split() for line in f)}
-	_words = set(_wordcounts.keys())
+  global _wordcounts
+  global _words
+  with open(_datapath+_language+'_vocab.txt', 'r') as f:
+    _wordcounts = {word: int(count) for word, count in (line.split() for line in f)}
+  _words = set(_wordcounts.keys())
 
 
 # sets dictionaries for mapping synsets between languages and synsets to lemmas
 @partial(_language_cache, names=['_synset2lemmas', '_synset2pwn', '_synset2lang'])
 def _set_synsetmaps():
-	global _synset2lemmas
-	global _synset2pwn
-	global _synset2lang
-	synset2lemmas = defaultdict(lambda: [])
-	for word, synsets in _word2synsets.items():
-		for synset in synsets:
-			synset2lemmas[synset].append(word)
-	_synset2lemmas = {}
-	_synset2pwn = {}
-	_synset2lang = {}
-	wordcounts = bool(_wordcounts)
-	for synset, lemmas in sorted(synset2lemmas.items()):
-		if wordcounts:
-			sortedlemmas = sorted(lemmas, key=_wordcounts.__getitem__, reverse=True)
-		else:
-			sortedlemmas = sorted(lemmas)
-		name = '.'.join([sortedlemmas[0], synset.split('.')[-2], '01'])
-		index = 1
-		while name in _synset2pwn:
-			index += 1
-			name = name[:-2] + (index<10)*'0'+str(index)
-		_synset2pwn[name] = synset
-		_synset2lang[synset] = name
-		_synset2lemmas[name] = ['.'.join([name, lemma]) for lemma in sortedlemmas]
+  global _synset2lemmas
+  global _synset2pwn
+  global _synset2lang
+  synset2lemmas = defaultdict(lambda: [])
+  for word, synsets in _word2synsets.items():
+    for synset in synsets:
+      synset2lemmas[synset].append(word)
+  _synset2lemmas = {}
+  _synset2pwn = {}
+  _synset2lang = {}
+  wordcounts = bool(_wordcounts)
+  for synset, lemmas in sorted(synset2lemmas.items()):
+    sortedlemmas = sorted(lemmas)
+    name = '.'.join([sortedlemmas[0], synset.split('.')[-2], '01'])
+    index = 1
+    while name in _synset2pwn:
+      index += 1
+      name = name[:-2] + (index<10)*'0'+str(index)
+    _synset2pwn[name] = synset
+    _synset2lang[synset] = name
+    _synset2lemmas[name] = ['.'.join([name, lemma]) for lemma in sortedlemmas]
 
 
 def language():
-	"""get current language setting"""
-	return _language
+  """get current language setting"""
+  return _language
 
 
 def set_language(language=_language, analyzer='auto'):
-	"""reset language setting"""
-	global _language
-	global _morphy
-	if _language != _langmap[language]:
-		_language = _langmap[language]
-		_load_word2synsets()
-		_load_wordcounts()
-		_set_synsetmaps()
-	if _language != 'en':
-		_morphy = Morphy(_language, analyzer).morphy
+  """reset language setting"""
+  global _language
+  global _morphy
+  if _language != _langmap[language]:
+    _language = _langmap[language]
+    _load_word2synsets()
+    _load_wordcounts()
+    _set_synsetmaps()
+  if _language != 'en':
+    _morphy = Morphy(_language, analyzer).morphy
--- pawn/morphy.py
+++ pawn/morphy.py
@@ -9,13 +9,13 @@ _langmap = {'fr': 'french', 'ru': 'russian'}
 _underscore2space = str.maketrans('_', ' ')
 _french_morphology = defaultdict(lambda: {})
 for suffix in ['ons', 'ez', 'ent', 'ais', 'ait', 'ions', 'iez', 'aient', 'ai', 'ont', 'eons', 'é', 'és', 'ée']:
-	_french_morphology[len(suffix)][suffix] = 'er'
+  _french_morphology[len(suffix)][suffix] = 'er'
 for suffix in ['ssons', 'ssez', 'ssent']:
-	_french_morphology[len(suffix)][suffix] = 'ir'
+  _french_morphology[len(suffix)][suffix] = 'ir'
 for suffix in ['s', 'e', 'es']:
-	_french_morphology[len(suffix)][suffix] = ''
+  _french_morphology[len(suffix)][suffix] = ''
 for char in ['l', 'n', 's']:
-	_french_morphology[3][2*char+'e'] = char
+  _french_morphology[3][2*char+'e'] = char
 _french_morphology[4]['eaux'] = 'eau'
 _max = max(_french_morphology.keys())
 _morph = pymorphy2.MorphAnalyzer()
@@ -24,83 +24,86 @@ _methods = {'<FakeDictionary>', '<HyphenatedWordsAnalyzer>', '<PunctuationAnalyz
 
 # wrapper to split input and join output by underscore
 def _stringsplit_wrapper(func):
-	@wraps(func)
-	def wrapper(string):
-		return '_'.join(func(chunk) for chunk in string.split('_'))
-	return wrapper
+  @wraps(func)
+  def wrapper(string):
+    return '_'.join(func(chunk) for chunk in string.split('_'))
+  return wrapper
 
 
 # tries to find root form of French word
 @_stringsplit_wrapper
 def _fr_morphy(string):
-	for length in reversed(range(1, min(len(string)-1, _max))):
-		suffix = _french_morphology[length].get(string[-length], None)
-		if suffix != None:
-			return string[:-length] + suffix
-	return string
+  for length in reversed(range(1, min(len(string)-1, _max))):
+    suffix = _french_morphology[length].get(string[-length], None)
+    if suffix != None:
+      return string[:-length] + suffix
+  return string
 
 
 # tries to find root form of Russian word
 @_stringsplit_wrapper
 def _ru_morphy(string):
-	scores = defaultdict(lambda: 0)
-	for form in self._morph.parse(token):
-		if not _methods.intersection(str(method[0]) for method in form.methods_stack):
-			score = form.score
-			normal_form = form.normal_form
-			if score >= .5:
-				return normal_form
-			scores[normal_form] += score
-			if scores[normal_form] >= .5:
-				return normal_form
-	if scores:
-		return max(scores.items(), key=itemgetter(1))[0]
-	return string
+  scores = defaultdict(lambda: 0)
+  for form in self._morph.parse(token):
+    if not _methods.intersection(str(method[0]) for method in form.methods_stack):
+      score = form.score
+      normal_form = form.normal_form
+      if score >= .5:
+        return normal_form
+      scores[normal_form] += score
+      if scores[normal_form] >= .5:
+        return normal_form
+  if scores:
+    return max(scores.items(), key=itemgetter(1))[0]
+  return string
 
 
 class Morphy:
 
 
-	# tries to find root form of word using TreeTagger lemmatizer
-	def _treetagger_morphy(self, string):
-		return '_'.join(tag.split('\t')[-1].lower() for tag in self._tagger.tag_text(string.translate(_underscore2space)))
+  # tries to find root form of word using TreeTagger lemmatizer
+  def _treetagger_morphy(self, string):
+    return '_'.join(tag.split('\t')[-1].lower() for tag in self._tagger.tag_text(string.translate(_underscore2space)))
 
-	# tries to find root form of word using Snowball Stemmer
-	@_stringsplit_wrapper
-	def _snowball_morphy(self, string):
-		stemmer = self._stemmer
-		return '_'.join(stemmer.stem(token) for token in string.split())
+  # tries to find root form of word using Snowball Stemmer
+  @_stringsplit_wrapper
+  def _snowball_morphy(self, string):
+    stemmer = self._stemmer
+    return '_'.join(stemmer.stem(token) for token in string.split())
 
-	# sets TreeTagger as the morphology analyzer
-	def _set_treetagger(self, language):
-		import treetaggerwrapper as ttw
-		self._tagger = ttw.TreeTagger(TAGLANG=language)
-		self.morphy = self._treetagger_morphy
+  # sets TreeTagger as the morphology analyzer
+  def _set_treetagger(self, language):
+    import treetaggerwrapper as ttw
+    try:
+      self._tagger = ttw.TreeTagger(TAGLANG=language)
+      self.morphy = self._treetagger_morphy
+    except ttw.TreeTaggerError:
+      raise(ImportError)
 
-	# sets SnowballStemmer as the morphology analyzer
-	def _set_snowball(self, language):
-		from nltk.stem import SnowballStemmer
-		self._stemmer = SnowballStemmer(_langmap[language])
-		self.morphy = self._snowball_morphy
+  # sets SnowballStemmer as the morphology analyzer
+  def _set_snowball(self, language):
+    from nltk.stem import SnowballStemmer
+    self._stemmer = SnowballStemmer(_langmap[language])
+    self.morphy = self._snowball_morphy
 
-	# sets custom morphology analyzer
-	def _set_morphy(self, language):
-		try:
-			self.morphy = globals()['_'+language+'_morphy']
-		except KeyError:
-			raise WordNetError('no custom morphology analyzer for ' + language + ' avaialable')
+  # sets custom morphology analyzer
+  def _set_morphy(self, language):
+    try:
+      self.morphy = globals()['_'+language+'_morphy']
+    except KeyError:
+      raise WordNetError('no custom morphology analyzer for ' + language + ' avaialable')
 
 
-	# determines which language and morphological analyzer to use
-	def __init__(self, language, analyzer):
+  # determines which language and morphological analyzer to use
+  def __init__(self, language, analyzer):
 
-		if analyzer == 'auto':
-			try:
-				self._set_treetagger(language)
-			except ImportError:
-				self._set_morphy(language)
-		else:
-			try:
-				getattr(self, '_set_'+analyzer)(language)
-			except AttributeError:
-				raise WordNetError('no morphology analyzer ' + analyzer + 'available')
+    if analyzer == 'auto':
+      try:
+        self._set_treetagger(language)
+      except ImportError:
+        self._set_morphy(language)
+    else:
+      try:
+        getattr(self, '_set_'+analyzer)(language)
+      except AttributeError:
+        raise WordNetError('no morphology analyzer ' + analyzer + 'available')
